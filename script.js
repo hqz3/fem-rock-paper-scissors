2714fd2e015301c7062b5b40302eb6ebd4d16726
hqz3 <heqzheng@gmail.com>
2023-04-13 17:59:29 -0400

    Create Game class and add eventListeners to handle each game chip selection

--- script.js
+++ script.js
@@ -33,3 +33,35 @@ document.addEventListener("click", (e) => {
     rules.hide();
   }
 });
+
+class Game {
+  constructor() {
+    this.board = document.querySelector(".game");
+    this.rock = document.querySelector(".game__chip--rock");
+    this.paper = document.querySelector(".game__chip--paper");
+    this.scissors = document.querySelector(".game__chip--scissors");
+    this.scoreEl = document.querySelector(".header__score-points");
+    this.score = 0;
+    this.choice = null;
+  }
+
+  hideGameBoard() {
+    this.board.classList.add("game--hide");
+  }
+
+  renderScore() {
+    this.scoreEl.textContent = this.score;
+  }
+}
+
+const game = new Game();
+
+const chips = document.querySelectorAll(".game__chip");
+chips.forEach((chip) => {
+  chip.addEventListener("click", () => {
+    game.choice = chip.dataset.choice;
+    game.hideGameBoard();
+  });
+});
+
+// game.hideGameBoard();
