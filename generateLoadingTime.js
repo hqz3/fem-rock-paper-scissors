852c4b80236b562e9ec23d4c5f21396b30c69e74
hqz3 <heqzheng@gmail.com>
2023-04-20 13:25:51 -0400

    Set a minimum loading time

--- generateLoadingTime.js
+++ generateLoadingTime.js
@@ -1,5 +1,5 @@
 const MAX_SECONDS = 2001;
 
 export function generateLoadingTime() {
-  return Math.floor(Math.random() * MAX_SECONDS);
+  return Math.max(500, Math.floor(Math.random() * MAX_SECONDS));
 }
